b4d629f3f52d22c42428d3ff7d84f8572ccf9318
LakshayBhandari <lakshaybhandari867@gmail.com>
2023-04-02 19:55:12 +0530

    3 rd commit

--- server/index.js
+++ server/index.js
@@ -9,8 +9,11 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 
 /* CONFIGURATIONS*/
@@ -43,11 +46,13 @@ const upload = multer({ storage });
 
 /*ROUTES WITH FILES*/ 
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts",verifyToken,upload.single("picture"),createPost);
 
 /*ROUTES */
 
 app.use("/auth",authRoutes);
 app.use("/users",userRoutes);
+app.use("/posts",postRoutes);
 
 
 /* MONGOOSE SETUP */
