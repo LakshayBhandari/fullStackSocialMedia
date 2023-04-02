45f6fc71de6035b98d9c1055d7d2d2ff39618174
LakshayBhandari <lakshaybhandari867@gmail.com>
2023-04-02 23:06:56 +0530

    3.3 commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import homePage from "./scenes/homePage/homePage";
+import loginPage from "./scenes/loginPage/loginPage";
+import profilePage from "./scenes/profilePage/profilePage";
 
 function App() {
   return (
     <div className="App">
-     hi
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={loginPage}/>
+        <Route path="/home" element={homePage}/>
+        <Route path="/profile/:userId" element={profilePage}/>
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
